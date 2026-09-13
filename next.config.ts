55b1ad1156a4b9c0f10a46fd89f4b6ce742dfccb
Shah Faisal <aalarelax@gmail.com>
2026-09-13 23:28:41 +0500

    pwa setting

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,27 @@
 import type { NextConfig } from "next";
-import withPWAInit from "@ducanh2912/next-pwa";
+// import withPWAInit from "@ducanh2912/next-pwa";
+import withPWA from "@ducanh2912/next-pwa";
+// next.config.ts
+
+
 
 const nextConfig: NextConfig = {
   // reactStrictMode: true,
   //   allowedDevOrigins: ['https://riderfrontend-4ijzxlpq7-aalacode.vercel.app/*'],
 
 };
-const withPWA = withPWAInit({
+// const withPWA = withPWAInit({
+//   dest: "public",
+//   disable: process.env.NODE_ENV === "development",
+//   register: true,
+//   // ⬇️ skipWaiting ko yahan workboxOptions ke andar rakhein
+//   workboxOptions: {
+//     skipWaiting: true,
+//   },
+// });
+
+export default withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  register: true,
-  // ⬇️ skipWaiting ko yahan workboxOptions ke andar rakhein
-  workboxOptions: {
-    skipWaiting: true,
-  },
-});
-export default nextConfig;
+})(nextConfig);
+// export default nextConfig;
